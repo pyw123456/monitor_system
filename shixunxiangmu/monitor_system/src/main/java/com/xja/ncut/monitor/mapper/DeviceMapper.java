f6897aeb58bb1182382d2fb5bd4a2e8391676b9f
Wang PengYu <136071377+pyw123456@users.noreply.github.com>
2026-09-10 09:20:14 +0800

    Delete shixunxiangmu directory

--- shixunxiangmu/monitor_system/src/main/java/com/xja/ncut/monitor/mapper/DeviceMapper.java
+++
@@ -1,9 +0,0 @@
-package com.xja.ncut.monitor.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.xja.ncut.monitor.entity.Device;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface DeviceMapper extends BaseMapper<Device> {
-}
